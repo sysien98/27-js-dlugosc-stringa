31fcdb7bca957a69f1b5a90a87049a357795cf19
Sylwia Sadowska <sylwiaxsadowska@gmail.com>
2018-10-11 16:25:34 +0200

    długość stringa

---
+++ main.js
@@ -0,0 +1,11 @@
+let zmienna1 = 'Uwielbiam JavaScript';
+let zmienna2 = 'Jestem świetnym programistą';
+function longer(zmienna1, zmienna2) {
+    if (zmienna1 > zmienna2) {
+    console.log('Uwielbiam JavaScript');
+}
+    else {
+        console.log('Jestem świetnym programistą')
+    }
+}
+longer();
